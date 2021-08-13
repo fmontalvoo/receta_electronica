55aab74fc4999075d214b2b4c3c15156d9703473
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-08-13 18:55:08 -0500

    Manejo de sesion del usuario.

--- app.py
+++ app.py
@@ -1,14 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
+app.secret_key = 'Rec3T@'
+
+@app.before_request
+def session_management():
+    session.permanent = True
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    usuario , rol = recuperar_usuario()
+    if usuario == 'desconocido':
+        return redirect(url_for('login'))
+    return render_template('index.html', data={'usuario':usuario, 'rol':rol})
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    usuario , rol = recuperar_usuario()
+    if usuario != 'desconocido':
+        return redirect(url_for('index'))
+    
+    if request.method == 'POST':
+        data = request.form
+        correo = data['correo']
+        clave = data['clave']
+        print(correo, clave)
+        if correo == 'test@email.com' and clave == '123':
+            session.clear()
+            session['correo'] = correo
+            session['usuario'] = 'Alan Brito'
+            session['rol'] = 'admin'
+            return redirect(url_for('index'))
     return render_template('usuario/login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    usuario = 'desconocido'
+    correo = 'ninguno'
+    rol = 'ninguno'
+    return redirect(url_for('login'))
+
+def recuperar_usuario():
+    try:
+        usuario = session['usuario']
+        correo = session['correo']
+        rol = session['rol']
+    except:
+        usuario = 'desconocido'
+        correo = 'ninguno'
+        rol = 'ninguno'
+    return (usuario, rol)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
